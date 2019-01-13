6c54fae4a25f1b6057554824f1f0ee93b2c9acc7
JP Hechter <joseph.hechter@gmail.com>
2019-01-13 13:31:11 -0600

    completed realpython's BeautifulSoup intro

---
+++ realpython_tutorial/test.py
@@ -0,0 +1,8 @@
+from bs4 import BeautifulSoup
+
+raw_html = open('contrived.html').read()
+html = BeautifulSoup(raw_html, 'html.parser')
+
+for p in html.select('p'):
+    if p['id'] == 'walrus':
+        print(p.text)
